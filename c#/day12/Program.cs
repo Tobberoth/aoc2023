7d9f9a3f4646de4d87aca78804cedcb96b1b1f01
Tobias Dersén <tobberoth@gmail.com>
2023-12-12 23:08:18 +0100

    Finally step 1 working

--- c#/day12/Program.cs
+++ c#/day12/Program.cs
@@ -1,76 +1,71 @@
-﻿var lines = File.ReadAllLines("input.txt");
-
-Console.WriteLine(lines.Select(GetAllArrangementsOver).Sum());
-
-int GetAllArrangementsOver(string line) {
-  var conditions = line.Split(' ')[0];
-  var groups = line.Split(' ')[1].Split(',').Select(int.Parse).ToList();
-  return GetAllArrangements(conditions, groups);
+﻿var lines = File.ReadAllLines("input.txt").ToList();
+lines = lines.Where(l => l.Length > 5).ToList();
+Dictionary<(string line, int group), int> GetAcceptedPositionsMemo = [];
+var total = 0;
+foreach (var line in lines) {
+  var condition = line.Split(' ')[0];
+  var groups = line.Split(" ")[1].Split(",").Select(int.Parse).ToList();
+  var arrangements = GetArrangements(condition, groups);
+  total += arrangements;
+  Console.WriteLine(arrangements);
+  //Console.WriteLine($"{condition} {string.Join(",", groups)} {arrangements}");
 }
+Console.WriteLine(total);
 
-int GetAllArrangements(string conditions, List<int> groups) {
-  if (groups.Sum() + groups.Count - 1 > conditions.Length) return 0;
+int GetArrangements(string line, List<int> groups) {
+  if (groups.Count == 1)
+    return GetAcceptedPositions(line, groups[0]);
 
-  var sum = 0;
   var first = groups[0];
   var rest = groups[1..];
-  // This is not enough, if there are large consecutive early this needs to be cut as well
-  // Need to cover enough as well, not just blanks
-  //var restCover = rest.Sum() + rest.Count;
-  var restCover = GetFirstFreeIndexBackwards(conditions, rest.Sum() + rest.Count - 1);
-  if (restCover == -1) return 0; // Rest doesn't fit
-  var space = conditions[..restCover];
-  Console.WriteLine($"{first} {space}");
 
-  // Find all possible solitons for first in space
-  // how
-  /*
-  if (space.Length == first)
-    sum += 1;
-  else if (Regex.IsMatch(space, @"#{" + first + "}"))
-    sum += 1;
-  else {
-
-  }
-  */
-
-  // Then add rest
-  var nextFree = FindFirstFreeIndex(conditions, first);
-  Console.WriteLine($"{rest} {conditions[nextFree..]}");
-  /*
-  sum += GetAllArrangements(conditions[nextFree..], groups[1..]);
-  */
-
-  return groups.Sum();
-}
-
-int FindFirstFreeIndex(string line, int spaceNeeded) {
-  var open = 0;
-  var i = 0;
-  foreach (char c in line) {
-    if (c == '?')
-      open++;
-    if (c == '#')
-      open = 1; // Reset since this must be covered
-    if (c == '.')
-      open = 0;
-    if (open == spaceNeeded)
-      return i+2; // Cover this and one open
-    i++;
-  }
-  return -1;
-}
-
-int GetFirstFreeIndexBackwards(string line, int spaceNeeded) {
-  // TODO: spaceneeded isn't accurate since it includes separating .
-  // Maybe count them separately
-
+  // for all accepted positions of first
   var sum = 0;
-  for (var i = line.Length-1; i >= 0; i--) {
-    if (line[i] == '#' || line[i] == '?')
-      sum++;
-    if (sum == spaceNeeded)
-      return i;
+  var foundFill = false;
+  for (var i = 0; i < line.Length; i++) {
+    var positionOk = IsPositionOk(line[i..], first);
+    if (positionOk) {
+      if (i+first+1 <= line.Length) {
+        var arrangements = GetArrangements(line[(i+first+1)..], rest);
+        if (arrangements > 0 && line[i..(i+first)].Any(c => c == '#'))
+          foundFill = true;
+        sum += arrangements;
+      }
+    } else if (foundFill && (i+first+1) < line.Length && line[i+first] == '.') break;
+    if (line[i] == '#')
+      break; // Found a guaranteed hit, can't go further
   }
-  return -1;
+  // add results from recursively calling rest of line with rest of groups
+  return sum;
+}
+
+int GetAcceptedPositions(string line, int group) {
+  if (line.Length < group) return 0;
+
+  if (GetAcceptedPositionsMemo.ContainsKey((line, group)))
+    return GetAcceptedPositionsMemo[(line, group)];
+
+  if (line.Contains('#')) {
+    var validStart = line.LastIndexOf('#')-group+1;
+    var validEnd = Math.Min(line.IndexOf('#')+group, line.Length);
+    if (validEnd < validStart) return 0;
+    var diff = Math.Abs(validStart - validEnd);
+    line = line[Math.Max(validStart, 0)..(validStart+diff)];
+  }
+  if (line == "") return 0;
+  if (GetAcceptedPositionsMemo.ContainsKey((line, group)))
+    return GetAcceptedPositionsMemo[(line, group)];
+  var sum = 0;
+  if (IsPositionOk(line, group)) {
+    sum++;
+  }
+  sum += GetAcceptedPositions(line[1..], group);
+  GetAcceptedPositionsMemo.Add((line, group), sum);
+  return sum;
+}
+
+bool IsPositionOk(string line, int group) {
+  if (line.Length < group) return false;
+  if (line.Length > group && line.Skip(group).Take(1).First() == '#') return false;
+  return line.Take(group).All(c => c == '?' || c == '#');
 }
