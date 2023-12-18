5659be7288e31fa0c41b7d72196b1e4f2fd26632
Tobias Dersén <tobberoth@gmail.com>
2023-12-18 09:52:52 +0100

    Day 18 step 1 and step 2 solved in different projects. Part 2 is miles better and could be updated to handle step 1 as well

---
+++ c#/day18-2/Program.cs
@@ -0,0 +1,30 @@
+﻿var hexes = File.ReadAllLines("input.txt").Select(l => l.Split(' ').Last());
+(long x, long y) current = (0, 0);
+List<(long x, long y)> Coords = [current];
+var sumSteps = 0;
+foreach (var hex in hexes) {
+  var steps = int.Parse(hex[2..^2], System.Globalization.NumberStyles.HexNumber);
+  sumSteps += steps;
+  var newCoord = hex[^2..^1] switch {
+    "0" => (current.x+steps, current.y),
+    "1" => (current.x, current.y+steps),
+    "2" => (current.x-steps, current.y),
+    "3" => (current.x, current.y-steps),
+  };
+  Coords.Add(newCoord);
+  current = newCoord;
+}
+
+Console.WriteLine(CalcAreaShoelace(Coords) + sumSteps/2+1);
+
+static double CalcAreaShoelace(List<(long x, long y)> coords) {
+  long sum = 0;
+  for (var i = 0; i < coords.Count-1; i++) {
+    var first = coords[i];
+    var second = coords[i+1];
+    var one = first.x * second.y;
+    var two = first.y * second.x;
+    sum += one - two;
+  }
+  return sum/2d;
+}
---
+++ c#/day18/Program.cs
@@ -0,0 +1,102 @@
+﻿List<List<string>> Map = new(10000) { new(10000) };
+Map[0].Add("#");
+var fromPos = (x: 0, y: 0);
+foreach (var line in File.ReadAllLines("input.txt")) {
+  var dir = line[0];
+  var steps = int.Parse(line.Split(' ')[1]);
+  var rgb = line.Split(' ')[2].TrimStart('(').TrimEnd(')');
+  fromPos = Draw(Map, dir, steps, rgb, fromPos.x, fromPos.y);
+}
+Fill(Map);
+Console.WriteLine(Map.Select(l => l.Count(c => c != "")).Sum());
+
+(int x, int y) Draw(List<List<string>> map, char direction, int steps, string rgb, int fromX, int fromY) {
+  switch (direction) {
+    case 'U':
+      for (var i = 0; i < steps; i++) {
+        var rowY = fromY - 1;
+        if (rowY < 0) {
+          Map.Insert(0, new List<string>(10000));
+          Map[0] = Map[1].Select(s => "").ToList();
+          rowY++;
+          Map[rowY][fromX] = rgb;
+        } else {
+          Map[rowY][fromX] = rgb;
+          fromY--;
+        }
+      }
+      break;
+    case 'R':
+      for (var i = 0; i < steps; i++) {
+        var colX = fromX + 1;
+        if (colX >= Map[0].Count) {
+          Map.ForEach(l => l.Add(""));
+          Map[fromY][colX] = rgb;
+        } else
+          Map[fromY][colX] = rgb;
+        fromX++;
+      }
+      break;
+    case 'D':
+      for (var i = 0; i < steps; i++) {
+        var rowY = fromY + 1;
+        if (rowY >= Map.Count) {
+          var newList = Map.Last().Select(s => "").ToList();
+          Map.Add(newList);
+          Map[rowY][fromX] = rgb;
+        } else 
+          Map[rowY][fromX] = rgb;
+        fromY++;
+      }
+      break;
+    case 'L':
+      for (var i = 0; i < steps; i++) {
+        var colX = fromX - 1;
+        if (colX < 0) {
+          Map.ForEach(l => l.Insert(0, ""));
+          colX = 0;
+          Map[fromY][colX] = rgb;
+        } else {
+          Map[fromY][colX] = rgb;
+          fromX--;
+        }
+      }
+    break;
+  }
+  return (fromX, fromY);
+}
+
+void Fill(List<List<string>> map) {
+  // Find point to start filling from
+  var height = map.Count;
+  var width = map[0].Count;
+  var midY = height/2 - 1;
+  if (height % 2 > 0)
+    midY++;
+  var insideX = 0;
+  for (var x = 0; x < width; x++) {
+    var cell = map[midY][x];
+    if (cell != "") {
+      insideX = x+1;
+      break;
+    }
+  }
+  
+  HashSet<(int x, int y)> PositionsToFill = [(insideX, midY)];
+  while (PositionsToFill.Count > 0) {
+    var currentPos = PositionsToFill.First();
+    PositionsToFill.Remove(currentPos);
+    // Get all adjacent
+    if (map[currentPos.y-1][currentPos.x] == "")
+      PositionsToFill.Add((currentPos.x, currentPos.y-1));
+    if (map[currentPos.y+1][currentPos.x] == "")
+      PositionsToFill.Add((currentPos.x, currentPos.y+1));
+    if (map[currentPos.y][currentPos.x-1] == "")
+      PositionsToFill.Add((currentPos.x-1, currentPos.y));
+    if (map[currentPos.y][currentPos.x+1] == "")
+      PositionsToFill.Add((currentPos.x+1, currentPos.y));
+    // Fill
+    map[currentPos.y][currentPos.x] = "#FFFFFF";
+  }
+  Console.WriteLine("Filling done");
+}
