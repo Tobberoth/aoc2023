f2c6c5e7cc9245147a37dba852902ddb820e17fb
Tobias Dersén <tobberoth@gmail.com>
2023-12-22 14:46:21 +0100

    Day 22 Done. Weird implementation, but decent speed. Day 21 step 2 still not completed.

--- c#/day21/Program.cs
+++ c#/day21/Program.cs
@@ -1,51 +1,40 @@
-﻿using System.Collections.ObjectModel;
+﻿Console.WriteLine(GetPossibleDestinations("input.txt", (65, 65), 1000));
+return;
 
-var Map = File.ReadAllLines("input.txt").ToList();
-const int stepsToTake = 64;
-(int x, int y, int stepsTaken) CurrentPosition = GetSPos(Map);
-HashSet<(int x, int y)> Reached = [(CurrentPosition.x, CurrentPosition.y)];
-Queue<(int x, int y, int stepsTaken)> PositionsToCheck = [];
-PositionsToCheck.Enqueue(CurrentPosition);
-while (true) {
-  CurrentPosition = PositionsToCheck.Dequeue();
-  if (CurrentPosition.stepsTaken > stepsToTake-1) break;
-  var valids = GetValidAdjacent(Reached, Map, (CurrentPosition.x, CurrentPosition.y));
+(long even, long uneven) GetPossibleDestinations(string filePath, (int x, int y) startLoc, int steps) {
+  var map = File.ReadAllLines(filePath).Select(l => l.Replace('S', '.')).ToList();
+  var mapWidth = map[0].Length;
+  var mapHeight = map.Count;
 
-  foreach (var valid in valids) {
-    Reached.Add(valid);
-    PositionsToCheck.Enqueue((valid.x, valid.y, CurrentPosition.stepsTaken + 1));
-  }
-}
-// Like a chess board, reachable positions toggle each step, get only valid ones
-Console.WriteLine(Reached.Where(r => (r.x + r.y) % 2 == 0).Count());
+  HashSet<(int x, int y)> Visited = [startLoc];
+  Queue<(int x, int y)> LocsToCheck = [];
+  LocsToCheck.Enqueue(startLoc);
 
-(int x, int y, int stepsTaken) GetSPos(List<string> map) {
-  for (var y = 0; y < map.Count; y++) {
-    for (var x = 0; x < map[0].Length; x++) {
-      if (map[y][x] == 'S')
-        return (x, y, 0);
+  var ret = (even: 0, uneven: 0);
+  var step = 0;
+  while (step <= steps) {
+    var queueSize = LocsToCheck.Count;
+    while (queueSize-- > 0) {
+      var currentLoc = LocsToCheck.Dequeue();
+      foreach (var adjacent in GetAdjacent(currentLoc, mapWidth, mapHeight)) {
+        if (!Visited.Contains(adjacent) && map[adjacent.y][adjacent.x] == '.') {
+          Visited.Add(adjacent);
+          LocsToCheck.Enqueue(adjacent);
+        }
+      }
+      if (step % 2 == 0 && (currentLoc.x + currentLoc.y) % 2 == 0)
+        ret = (++ret.even, ret.uneven);
+      else if (step % 2 == 1 && (currentLoc.x + currentLoc.y) % 2 == 1)
+        ret = (ret.even, ++ret.uneven);
     }
+    step++;
   }
-  throw new InvalidOperationException("Could not find S position in input");
+  return ret;
 }
 
-Collection<(int x, int y)> GetValidAdjacent(HashSet<(int x, int y)> reached, List<string> map, (int x, int y) fromPos) {
-  Collection<(int x, int y)> ret = [];
-  // Check up
-  var up = (fromPos.x, y: fromPos.y-1);
-  if (!reached.Contains(up) && map[up.y][up.x] == '.')
-    ret.Add(up);
-  // Check right
-  var right = (x: fromPos.x+1, fromPos.y);
-  if (!reached.Contains(right) && map[right.y][right.x] == '.')
-    ret.Add(right);
-  // check down
-  var down = (fromPos.x, y: fromPos.y+1);
-  if (!reached.Contains(down) && map[down.y][down.x] == '.')
-    ret.Add(down);
-  // check left
-  var left = (x: fromPos.x-1, fromPos.y);
-  if (!reached.Contains(left) && map[left.y][left.x] == '.')
-    ret.Add(left);
-  return ret;
+IEnumerable<(int x, int y)> GetAdjacent((int x, int y) currentLoc, int mapWidth, int mapHeight) {
+  if (currentLoc.y - 1 >= 0) yield return (currentLoc.x, currentLoc.y-1);
+  if (currentLoc.y + 1 < mapHeight) yield return (currentLoc.x, currentLoc.y+1);
+  if (currentLoc.x - 1 >= 0) yield return (currentLoc.x-1, currentLoc.y);
+  if (currentLoc.x + 1 < mapWidth) yield return (currentLoc.x+1, currentLoc.y);
 }
---
+++ c#/day22/Program.cs
@@ -0,0 +1,134 @@
+﻿HashSet<Vector3D> AllPositions = [];
+var Bricks = File.ReadAllLines("input.txt").Select(l => new Brick(l)).ToList();
+Bricks = Bricks.OrderBy(b => b.Positions.Min(p => p.Z)).ToList();
+foreach (var brick in Bricks)
+  foreach (var pos in brick.Positions)
+    AllPositions.Add(pos);
+var fell = int.MaxValue;
+while (fell > 0) {
+  fell = DropAll(Bricks, AllPositions);
+}
+Bricks = [.. Bricks.OrderBy(b => b.Positions.Min(p => p.Z))];
+
+// STEP 1
+var sum = 0;
+foreach (var brick in Bricks) {
+  foreach (var pos in brick.Positions)
+    AllPositions.Remove(pos);
+  List<Brick> tempStack = [..Bricks];
+  var index = tempStack.IndexOf(brick);
+  tempStack[index..].ForEach(b => b.Stable = false);
+  tempStack.Remove(brick);
+  if (!TestDrop(tempStack))
+    sum++;
+  foreach (var pos in brick.Positions)
+    AllPositions.Add(pos);
+  
+}
+Console.WriteLine(sum);
+
+// STEP 2
+long sum2 = 0;
+foreach (var obrick in Bricks) {
+  var thisBricks = new List<Brick>();
+  foreach (var brickx in Bricks)
+    thisBricks.Add(brickx.Copy());
+  HashSet<Vector3D> allPositionsCopy = [..AllPositions];
+  foreach (var pos in obrick.Positions)
+    allPositionsCopy.Remove(pos);
+  List<Brick> tempStack = [..thisBricks];
+  var index = tempStack.FindIndex(b => b.Positions.SequenceEqual(obrick.Positions));
+  tempStack[index..].ForEach(b => b.Stable = false);
+  tempStack.RemoveAt(index);
+  sum2 += DropAll(tempStack, allPositionsCopy);
+}
+Console.WriteLine(sum2);
+
+int DropAll(List<Brick> bricks, HashSet<Vector3D> allPositions) {
+  var fell = 0;
+  foreach (var brick in bricks) {
+    var newPos = brick.Drop();
+    if (newPos.Count > 0) {
+      foreach (var pos in brick.Positions)
+        allPositions.Remove(pos);
+      var collided = CheckAllCollisions(newPos, allPositions);
+      if (collided) {
+        foreach (var pos in brick.Positions)
+          allPositions.Add(pos);
+        brick.Stable = true;
+      } else {
+        fell++;
+        brick.Positions = newPos;
+        foreach (var pos in newPos)
+          allPositions.Add(pos);
+      }
+    }
+  }
+  return fell;
+}
+
+bool TestDrop(List<Brick> bricks) {
+  foreach (var brick in bricks) {
+    var newPos = brick.Drop();
+    if (newPos.Count > 0) {
+      foreach (var pos in brick.Positions)
+        AllPositions.Remove(pos);
+      var collided = CheckAllCollisions(newPos, AllPositions);
+      foreach (var pos in brick.Positions)
+        AllPositions.Add(pos);
+      if (!collided) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool CheckAllCollisions(List<Vector3D> newPos, HashSet<Vector3D> Filled) =>
+  newPos.Any(Filled.Contains);
+
+class Brick {
+  public List<Vector3D> Positions { get; set; } = [];
+  public bool Stable { get; set; }
+  public Brick() { }
+  public Brick(string line) {
+    var start = line.Split('~')[0].Split(',').Select(int.Parse).ToList();
+    var end  = line.Split('~')[1].Split(',').Select(int.Parse).ToList();
+    for (var pos = 0; pos < 3; pos++) {
+      if (start[pos] == end[pos]) continue;
+      var diff = end[pos] - start[pos];
+      for (var step = 0; step <= diff; step++) {
+        switch (pos) {
+          case 0:
+            Positions.Add(new Vector3D(start[0]+step, start[1], start[2]));
+            break;
+          case 1:
+            Positions.Add(new Vector3D(start[0], start[1]+step, start[2]));
+            break;
+          default:
+            Positions.Add(new Vector3D(start[0], start[1], start[2]+step));
+            break;
+        }
+      }
+    }
+    if (Positions.Count == 0)
+      Positions.Add(new Vector3D(start[0], start[1], start[2]));
+  }
+
+  public List<Vector3D> Drop() {
+    if (Stable) return [];
+    var ret = Positions.Select(p => new Vector3D(p.X, p.Y, p.Z-1)).ToList();
+    if (ret.All(p => p.Z > 0)) return ret;
+    Stable = true; // Grounded
+    return [];
+  }
+
+  public Brick Copy() {
+    var newBrick = new Brick { Stable = Stable };
+    foreach (var pos in Positions)
+      newBrick.Positions.Add(pos);
+    return newBrick;
+  }
+}
+
+record Vector3D(int X, int Y, int Z);
